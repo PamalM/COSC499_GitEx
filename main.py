697713a0923c665233bfef97c390d57f8e2ea9ac
Pamal Mangat <mangatpamal@gmail.com>
2020-09-11 12:54:26 -0700

    - Master branch is complete. - Master branch's feature: Sorting an Integer array

---
+++ main.py
@@ -0,0 +1,19 @@
+# Student Name & #: Pamal Mangat - 62994785
+# Course: COSC 499 - Capstone Project; (GitHub Ex #1)
+
+# Empty array to be filled after user prompt.
+array = []
+
+# Prompt user for integers to fill array.
+print('\nEnter 5 integers to fill the empty array:')
+element = None
+# Append user integers into array.
+for x in range(1, 6):
+    element = int(input('Integer [{0}] = '.format(x)))
+    array.append(element)
+
+# View initialized array before sorting.
+print('\nThe array has been initialized:\n' + str(array))
+
+# View initialized array after sorting.
+print('\nInteger array after sorting:\n' + str(sorted(array)))
