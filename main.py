afa81d4b06d96cda0dc5f47c972a776c42e0f880
Pamal Mangat <mangatpamal@gmail.com>
2020-09-11 15:12:13 -0700

    - branch2 complete

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 # Student Name & #: Pamal Mangat - 62994785
 # Course: COSC 499 - Capstone Project; (GitHub Ex #1)
 
+
+def average(list):
+    total = 0
+    for y in list:
+        total += int(y)
+    length = len(array)
+    return round(total / length, 2)
+
+
 # Empty array to be filled after user prompt.
 array = []
 
@@ -13,9 +22,9 @@ for x in range(1, 6):
     array.append(element)
 
 # View initialized array before stats.
-print('\nThe array has been initialized:\n' + str(array))
+print('\nThe array has been initialized:\n', str(array))
 
 # View initialized array's stats.
-print('\nInteger array [Min-Value]:\n' + str(min(array)))
-print('\nInteger array [Min-Value]:\n' + str(min(array)))
-print('\nInteger array [Average]:\n' + str(sum(array) / len(array)))
+print('\nInteger array [Max-Value]: ', max(array))
+print('Integer array [Min-Value]: ', min(array))
+print('Integer array [Average]: ', average(array))
